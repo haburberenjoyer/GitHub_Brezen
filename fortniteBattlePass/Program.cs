85e4e0597e52bdfecb634367feae6e0f8c90a0c1
vitek <vitek@DESKTOP-M7G96AE>
2022-04-03 19:12:36 +0200

    Přidejte soubory projektu.

---
+++ fortniteBattlePass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fortniteBattlePass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            int cislo1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" ");
+            int cislo2 = Convert.ToInt32(Console.ReadLine());
+            int[] cisla = { cislo1, cislo2 };
+            Console.Clear();
+            int max = cisla.Max();
+            int min = cisla.Min();
+            Console.WriteLine(max + ", " + min);
+            Console.ReadLine();
+        }
+    }
+}
